e3b1e778ec8df7b0fc683a3e1fc5ddf36a33aceb
Alexey Ternavskiy <oternavskyi@lohika.com>
2020-01-29 22:59:14 +0200

    Add closing cursor.

--- db/queries.py
+++ db/queries.py
@@ -1,26 +1,26 @@
+from contextlib import closing
 from sqlite3 import Connection as SqliteConnection
 from typing import Dict
 
 
 def add_user(conn: SqliteConnection,
              user_data: Dict) -> None:
-
     external_id = user_data.pop('id')
     user_data['external_id'] = external_id
 
-    cursor = conn.cursor()
-    cursor.execute("""
-        insert into users(external_id, first_name, last_name, username, is_bot)
-        select :external_id, :first_name, :last_name, :username, :is_bot
-        where not exists(select 1 from users where external_id = :external_id)
-    """, user_data)
-    conn.commit()
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            insert into users(external_id, first_name, last_name, username, is_bot)
+            select :external_id, :first_name, :last_name, :username, :is_bot
+            where not exists(select 1 from users where external_id = :external_id)
+        """, user_data)
+        conn.commit()
 
 
 def delete_user(conn: SqliteConnection,
                 user_id: int) -> None:
-    cursor = conn.cursor()
-    cursor.execute("""
-        delete from users where external_id = :external_id
-    """, {'external_id': user_id})
-    conn.commit()
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            delete from users where external_id = :external_id
+        """, {'external_id': user_id})
+        conn.commit()
