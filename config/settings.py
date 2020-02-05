28c7106ec48a664b580c527c6f09efcf07a34089
Alexey Ternavskiy <oternavskyi@lohika.com>
2020-02-05 18:15:35 +0200

    Add settings.py

---
+++ config/settings.py
@@ -0,0 +1,24 @@
+import pathlib
+
+__all__ = ['settings']
+
+from utils import read_config
+
+
+class Settings:
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Settings, cls).__new__(cls)
+        return cls.instance
+
+    ROOT_DIR = pathlib.Path(__file__).parents[1]
+    CONFIG_PATH = ROOT_DIR.joinpath('config.ini')
+    REQUIRED_FIELDS = ['BotToken', 'WebHookUrl']
+    CONFIG = read_config(CONFIG_PATH, REQUIRED_FIELDS)
+    TOKEN = CONFIG.get(CONFIG.default_section, 'BotToken')
+    WEB_HOOK_URL = CONFIG.get(CONFIG.default_section, 'WebHookUrl')
+    DEBUG = CONFIG.getboolean(CONFIG.default_section, 'Debug')
+    MEDIA = 'media'
+
+
+settings = Settings()
