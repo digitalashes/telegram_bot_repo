29f311b6f023ee816f64e7cd6b517b5d49815227
Alexey Ternavskiy <oternavskyi@lohika.com>
2020-02-05 18:52:07 +0200

    Add db queries.

--- db/queries.py
+++ db/queries.py
@@ -1,6 +1,11 @@
 from contextlib import closing
 from sqlite3 import Connection as SqliteConnection
 from typing import Dict
+from typing import List
+from typing import Optional
+
+from .models import Image
+from .models import User
 
 
 def add_user(conn: SqliteConnection,
@@ -23,6 +28,18 @@ def add_user(conn: SqliteConnection,
         conn.commit()
 
 
+def get_user(conn: SqliteConnection,
+             user_id: int) -> Optional[User]:
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            select *
+            from users
+            where external_id = :external_id
+        """, {'external_id': user_id})
+        row = cursor.fetchone()
+        return User(**row) if row else None
+
+
 def delete_user(conn: SqliteConnection,
                 user_id: int) -> None:
     with closing(conn.cursor()) as cursor:
@@ -30,3 +47,49 @@ def delete_user(conn: SqliteConnection,
             delete from users where external_id = :external_id
         """, {'external_id': user_id})
         conn.commit()
+
+
+def add_image(conn: SqliteConnection,
+              image_data: Dict) -> None:
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            insert into images(path, user_id)
+            select :image_path, :user_id
+            where not exists(select 1 from images where user_id = :user_id and path = :image_path)
+        """, image_data)
+        conn.commit()
+
+
+def get_images(conn: SqliteConnection,
+               user_id: int) -> Optional[List[Image]]:
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            select *
+            from images
+            where user_id = :user_id
+        """, {'user_id': user_id})
+        rows = cursor.fetchall()
+        return [Image(**row) for row in rows] if rows else []
+
+
+def delete_image(conn: SqliteConnection,
+                 image_id: int) -> None:
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            delete from images where id = :image_id
+        """, {'image_id': image_id})
+        conn.commit()
+
+
+def get_image(conn: SqliteConnection,
+              user_id: int,
+              image_id: int) -> Optional[Image]:
+    with closing(conn.cursor()) as cursor:
+        cursor.execute("""
+            select *
+            from images
+            where user_id = :user_id and id = :image_id
+        """, {'user_id': user_id, 'image_id': image_id})
+
+        row = cursor.fetchone()
+        return Image(**row) if row else None
