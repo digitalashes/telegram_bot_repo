123c34211abaeaf5dd77282b164ba89b6ab27f75
Alexey Ternavskiy <oternavskyi@lohika.com>
2020-02-05 18:16:42 +0200

    Refactoring.

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-import uvloop
+try:
+    import uvloop
+
+    use_uvloop = True
+except ImportError:
+    use_uvloop = False
+
 from aiohttp.web import run_app
 
 from bot import setup
+from config import settings
 from utils import make_logger
-from utils import read_config
 from web import make_app
 
 
 def main():
-    config_path = 'config.ini'
-    required_fields = ['BotToken', 'WebHookUrl']
-    config = read_config(config_path, required_fields)
-
-    token = config.get(config.default_section, 'BotToken')
-    web_hook_url = config.get(config.default_section, 'WebHookUrl')
-    debug = config.getboolean(config.default_section, 'Debug')
-
-    logger = make_logger(debug, name='BotLogger')
-    dispatcher = setup(token, web_hook_url, logger)
+    logger = make_logger(settings.DEBUG, name='BotLogger')
+    dispatcher = setup(settings.TOKEN, settings.WEB_HOOK_URL, logger)
 
     run_app(make_app(dispatcher))
 
 
 if __name__ == '__main__':
-    uvloop.install()
+    if use_uvloop:
+        uvloop.install()
     main()
